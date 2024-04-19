9481ee61ec49653ce2233f4e5318bd90bb9ff802
Rick Picolli <ricardopicolli@gmail.com>
2024-04-19 15:17:46 -0700

    initial commit

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get the inputs from EditText
+        // create a variable and associate it to the user input
+
+
+
+
+
     }
 }
