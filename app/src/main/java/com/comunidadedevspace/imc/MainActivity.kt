045bdb30f74ac7099e932076b8ada8b244f860a2
Rick Picolli <ricardopicolli@gmail.com>
2024-04-19 15:26:36 -0700

    adding button listener

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,6 +2,8 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +13,15 @@ class MainActivity : AppCompatActivity() {
         // get the inputs from EditText
         // create a variable and associate it to the user input
 
+        val userWeight = findViewById<TextInputEditText>(R.id.edt_weight)
+        val userHeight = findViewById<TextInputEditText>(R.id.edt_height)
+        val btnGenerateBmi = findViewById<Button>(R.id.btn_generate_bmi)
 
+        btnGenerateBmi.setOnClickListener {
+            val weightInput = userWeight.text
+            val heightInput = userHeight.text
+            println("test btn" + weightInput + " " + heightInput)
+        }
 
 
 
